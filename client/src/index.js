73cf991c4dfec24134dadb64f50d839aecf6c534
Nadim <hussennadim49@gmail.com>
2022-12-09 16:41:42 +0600

    react with graphql 🤔🤔

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {gql, ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
+const client = new ApolloClient({
+  url:'http://localhost:8000/graphql',
+  cache: new InMemoryCache()
+})
+client.query({ //languageQuery is any name just unique
+  query: gql`
+    query languageQuery{
+      language{
+        id
+      }
+    }
+  `
+}).then(result=>console.log(result+'hee0')).catch((err)=>{
+  console.log(err);
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <ApolloProvider client={client}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
